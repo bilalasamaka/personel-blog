7d160b3fc205c2513323fd5fbc5719b4a42003cd
Bilal Asamaka <bilal.asamaka@solvoyo.local>
2024-03-13 01:03:40 +0300

    added text editor

---
+++ build/precache-manifest.b9a271ada77a6689e4a1fdbcbd134f18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c431e1da58501d388ce23b1f49b4828",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b793d930f77496b63c45",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "b793d930f77496b63c45",
+    "url": "/static/js/2.54d7a696.chunk.js"
+  },
+  {
+    "revision": "4665489cf99eb53230fe5abcba1e4a2e",
+    "url": "/static/js/2.54d7a696.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50c58d7b591705de5cb3",
+    "url": "/static/js/main.12105202.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "fae0e57fe07560137edcf6df6d51ca86",
+    "url": "/static/media/post_1.fae0e57f.png"
+  },
+  {
+    "revision": "aa8d55a507aedd6229b47c9d146dc299",
+    "url": "/static/media/sample.aa8d55a5.md"
+  }
+]);
